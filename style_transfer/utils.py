085c229a6615e8b8433fc7580d0ec0f0441fade0
Vachan D A <vaas4627@colorado.edu>
2019-05-07 08:25:16 -0600

    Bug fix in utils.

--- style_transfer/utils.py
+++ style_transfer/utils.py
@@ -54,7 +54,6 @@ class Utils(object):
 
 	@staticmethod
 	def save_image(filename, data):
-	    img = data.clone().clamp(0, 255).numpy()
 	    img = img.transpose(1, 2, 0).astype("uint8")
 	    img = Image.fromarray(img)
 	    img.save(filename)
