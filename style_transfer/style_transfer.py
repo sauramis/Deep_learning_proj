9f547297cb91d91e40ef9f69c08886bea27b9635
Vachan D A <vaas4627@colorado.edu>
2019-05-07 18:30:17 -0600

    Changes the argparser.

--- style_transfer/style_transfer.py
+++ style_transfer/style_transfer.py
@@ -62,50 +62,42 @@ def stylize(args):
 
 def define_module_args():
 	main_arg_parser = argparse.ArgumentParser(description="parser for style transfer")
-	subparsers = main_arg_parser.add_subparsers(title="subcommands", dest="subcommand")
-	model_arg_parser = subparsers.add_parser("model", help="parser for model arguments")
-	model_arg_parser.add_argument("--method", help="type of style transfer", type=str, required=True, default="original")
-	model_arg_parser.add_argument("--epochs", help="number of epochs for evaluation", type=int, default=10)
-	model_arg_parser.add_argument("--log-path", help="path to log directory",type=str, required=True)
-	model_arg_parser.add_argument("--style-image", help="path to the style image",
-		type=str, required=True, default="images/style-images/mosaic.jpg"
-	)
-	model_arg_parser.add_argument("--segmentation", help="segment the content image",
-		type=bool, default=False
-	)
-	model_arg_parser.add_argument("--image-size", help="size of training images, default is 256 X 256",
-		type=int, default=256
-	)
-	model_arg_parser.add_argument("--content-image", help="path to the content image",
-		type=str, required=True, default="images/content/mosaic.jpg"
-	)
-	model_arg_parser.add_argument("--output-image", help="path for saving the output image",
-		type=str, required=True
-	)
-	model_arg_parser.add_argument("--cuda", help="set it to 1 for running on GPU, 0 for CPU", 
-    	type=int, required=True
-	)
-	model_arg_parser.add_argument("--content-weight", help="weight for content-loss, default is 1e5",
-    	type=float, default=1e5
-  	)
-	model_arg_parser.add_argument("--style-weight", help="weight for style-loss, default is 1e10", 
-  		type=float, default=1e10
-	)
+	main_arg_parser.add_argument("--transfer-method", help="type of style transfer", type=str, required=True, default="original")
+	main_arg_parser.add_argument("--epochs", help="number of epochs for evaluation", type=int, default=10)
+	main_arg_parser.add_argument("--style-image", help="path to the style image", type=str, required=True, default="images/style-images/mosaic.jpg")
+	main_arg_parser.add_argument("--segmentation", help="segment the content image", type=bool, default=False)
+	main_arg_parser.add_argument("--image-size", help="size of training images, default is 256 X 256", type=int, default=256)
+	main_arg_parser.add_argument("--content-image", help="path to the content image", type=str, required=True, default="images/content/mosaic.jpg")
+	main_arg_parser.add_argument("--cuda", help="set it to 1 for running on GPU, 0 for CPU", type=int, required=True)
+	main_arg_parser.add_argument("--content-weight", help="weight for content-loss, default is 5",type=float, default=5)
+	main_arg_parser.add_argument("--style-weight", help="weight for style-loss, default is 1e2", type=float, default=1e2)
+	main_arg_parser.add_argument("--tv-weight", help="weight for TV-loss, default is 1e-3", type=float, default=1e-3)
+	main_arg_parser.add_argument("--tv-weight", help="weight for TV-loss, default is 1e-3", type=float, default=1e-3)
+	main_arg_parser.add_argument("--learning-rate", help="Learning Rate", type=float, default=0.08)
+	main_arg_parser.add_argument("--target-rand", help="Initialize with random image", type=bool, default=False)
+	main_arg_parser.add_argument("--show-transitions", help="Set to show intermediate transitions", type=bool, default=False)
+	main_arg_parser.add_argument("--optimizer", help="type of optimizer to be used", type=str, default="Adam")
+	main_arg_parser.add_argument("--interval", help="epoch interval for showing intermediate transitions", type=int, default=100)
+	main_arg_parser.add_argument("--tv-weight", help="weight for TV-loss, default is 1e-3", type=float, default=1e-3)
+	main_arg_parser.add_argument("--content-scale", help="set the content image scale for Fast style transfer", type=float, default=1.0)
+	main_arg_parser.add_argument("--select-model-type", help="select the pre-trained style model type (candy|mosaic|rain_princess|udnie).", type=str, default=1e-3, required=True)
+
+
 
 	return main_arg_parser.parse_args()
 
 def main():
-	# args = define_module_args()
+	args = define_module_args()
 
-	# if args.subcommand is None:
-	# 	print("Error: specify model")
-	# 	sys.exit(1)
+	if args.subcommand is None:
+		print("Error: specify model")
+		sys.exit(1)
 
-	# if args.cuda and not torch.cuda.is_available():
-	# 	print("Error: cuda is not available, try it on CPU")
-	# 	sys.exit(1)
+	if args.cuda and not torch.cuda.is_available():
+		print("Error: cuda is not available, try it on CPU")
+		sys.exit(1)
 		
-	args = argparse.Namespace()
+	# args = argparse.Namespace()
 	# args.content_weight = 5
 	# args.style_weight = 1e2
 	# args.tv_weight = 1e-3
@@ -124,13 +116,12 @@ def main():
 
 	# For Fast Style Transfer
 
-	args.transfer_method = 2
-	args.segmentation = True
-	args.content_image = '/content/content-sample.jpg'
-	args.content_scale = 1.0
-	args.model = 2
-	args.style_model_type = "rain_princess"
-	args.cuda = 1
+	# args.transfer_method = 2
+	# args.segmentation = True
+	# args.content_image = '/content/content-sample.jpg'
+	# args.content_scale = 1.0
+	# args.style_model_type = "rain_princess"
+	# args.cuda = 1
 
 	stylize(args)
 
