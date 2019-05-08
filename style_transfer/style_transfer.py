be709d47475138f6e360d375845271630b66e782
Vachan D A <vaas4627@colorado.edu>
2019-05-07 18:38:37 -0600

    Changes to the images.

--- style_transfer/style_transfer.py
+++ style_transfer/style_transfer.py
@@ -33,6 +33,8 @@ def stylize(args):
 		segmentation_model = Segmentation(args_dict)
 		_fr_img, seg_results = segmentation_model.inference(args.content_image)
 
+	transformed_image_tensor = None
+
 	if args.transfer_method == 1:
 		c_img = Utils.load_image(args.content_image)
 		style_img = Utils.load_image(args.style_image)
@@ -43,6 +45,8 @@ def stylize(args):
 	elif args.transfer_method == 2:
 		transformer = FastStyleTransfer(args_dict, device)
 		transformed_image_tensor = transformer.inference()
+	else:
+		raise Exception("Model not implemented.")
 
 	output_filename = "outputs/results_" + str(int(time.time())) + ".png"
 	output_filename = os.path.join(base_path, output_filename)
@@ -60,12 +64,20 @@ def stylize(args):
 		else:
 			Utils.save_image(output_filename, transformed_image_tensor)
 
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 def define_module_args():
 	main_arg_parser = argparse.ArgumentParser(description="parser for style transfer")
-	main_arg_parser.add_argument("--transfer-method", help="type of style transfer", type=str, required=True, default="original")
+	main_arg_parser.add_argument("--transfer-method", help="type of style transfer", type=int, required=True)
 	main_arg_parser.add_argument("--epochs", help="number of epochs for evaluation", type=int, default=10)
-	main_arg_parser.add_argument("--style-image", help="path to the style image", type=str, required=True, default="images/style-images/mosaic.jpg")
-	main_arg_parser.add_argument("--segmentation", help="segment the content image", type=bool, default=False)
+	main_arg_parser.add_argument("--style-image", help="path to the style image", type=str, default="images/style-images/black_lines.jpg")
+	main_arg_parser.add_argument("--segmentation", help="segment the content image", type=str2bool, default=False)
 	main_arg_parser.add_argument("--image-size", help="size of training images, default is 256 X 256", type=int, default=256)
 	main_arg_parser.add_argument("--content-image", help="path to the content image", type=str, required=True, default="images/content/mosaic.jpg")
 	main_arg_parser.add_argument("--cuda", help="set it to 1 for running on GPU, 0 for CPU", type=int, required=True)
@@ -73,26 +85,21 @@ def define_module_args():
 	main_arg_parser.add_argument("--style-weight", help="weight for style-loss, default is 1e2", type=float, default=1e2)
 	main_arg_parser.add_argument("--tv-weight", help="weight for TV-loss, default is 1e-3", type=float, default=1e-3)
 	main_arg_parser.add_argument("--learning-rate", help="Learning Rate", type=float, default=0.08)
-	main_arg_parser.add_argument("--target-rand", help="Initialize with random image", type=bool, default=False)
-	main_arg_parser.add_argument("--show-transitions", help="Set to show intermediate transitions", type=bool, default=False)
+	main_arg_parser.add_argument("--target-rand", help="Initialize with random image", type=str2bool, default=False)
+	main_arg_parser.add_argument("--show-transitions", help="Set to show intermediate transitions", type=str2bool, default=False)
 	main_arg_parser.add_argument("--optimizer", help="type of optimizer to be used", type=str, default="Adam")
 	main_arg_parser.add_argument("--interval", help="epoch interval for showing intermediate transitions", type=int, default=100)
 	main_arg_parser.add_argument("--content-scale", help="set the content image scale for Fast style transfer", type=float, default=1.0)
-	main_arg_parser.add_argument("--select-model-type", help="select the pre-trained style model type (candy|mosaic|rain_princess|udnie).", type=str, default=1e-3, required=True)
+	main_arg_parser.add_argument("--style-model-type", help="select the pre-trained style model type (candy|mosaic|rain_princess|udnie).", type=str, default=1e-3)
 
 	return main_arg_parser.parse_args()
 
 def main():
 	args = define_module_args()
 
-	if args.subcommand is None:
-		print("Error: specify model")
-		sys.exit(1)
-
 	if args.cuda and not torch.cuda.is_available():
 		print("Error: cuda is not available, try it on CPU")
 		sys.exit(1)
-
 
 	if args.transfer_method == 1:
 		args.style_weights = get_style_weights()
